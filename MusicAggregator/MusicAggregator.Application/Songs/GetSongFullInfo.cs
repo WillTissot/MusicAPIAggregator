e09caadd4223ed7aae049a79ec101fb7e898fef6
Vasilis <vasilis.softsis@ebiscon.de>
2026-06-16 16:39:09 +0300

    create the get song service

---
+++ MusicAggregator/MusicAggregator.Application/Songs/GetSongFullInfo.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using MusicAggregator.Application.Abstractions;
+using MusicAggregator.Application.Models;
+
+namespace MusicAggregator.Application.Songs
+{
+    internal class GetSongFullInfo : IGetSongFullInfo
+    {
+        private readonly ITrackProvider _trackProvider;
+        private readonly IArtistProvider _artistProvider;
+        private readonly ILyricsProvider _lyricsProvider;
+
+        private readonly ILogger<GetSongFullInfo> _logger;
+
+        public GetSongFullInfo(ITrackProvider trackProvider, IArtistProvider artistProvider, ILyricsProvider lyricsProvider, ILogger<GetSongFullInfo> logger)
+        {
+            _trackProvider = trackProvider;
+            _artistProvider = artistProvider;
+            _lyricsProvider = lyricsProvider;
+            _logger = logger;
+        }
+
+        public async Task<SongPage> GetSongFullInfoAsync(string track, string artist, CancellationToken ct)
+        {
+
+            Task<TrackInfo?> trackTask = _trackProvider.SearchTrackAsync(track, artist, ct);
+            Task<ArtistInfo?> artistTask = _artistProvider.GetArtistAsync(artist, ct);
+            Task<LyricsInfo?> lyricsTask = _lyricsProvider.GetLyricsInfoAsync(track, artist, ct);
+
+            await Task.WhenAll(trackTask, artistTask, lyricsTask);
+
+            _logger.LogDebug("Fetching info for track, artist and lyrics is finished.");
+
+            TrackInfo? trackInfo = trackTask.Result;
+            ArtistInfo? artistInfo = artistTask.Result;
+            LyricsInfo? lyricsInfo = lyricsTask.Result;
+
+            SongPage songPage = new()
+            {
+                Query = $"Info for track:{track} and artist: {artist}",
+                Sources = new()
+                {
+                    Artist = artistInfo is not null,
+                    Lyrics = lyricsInfo is not null,
+                    Track = trackInfo is not null
+                },
+                Artist = artistInfo,
+                Lyrics = lyricsInfo,
+                Track = trackInfo
+
+            };
+
+            return songPage;
+        }
+    }
+}
---
+++ MusicAggregator/MusicAggregator.Application/Songs/IGetSongFullInfo.cs
@@ -0,0 +1,14 @@
+﻿using MusicAggregator.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicAggregator.Application.Songs
+{
+    internal interface IGetSongFullInfo
+    {
+        Task<SongPage> GetSongFullInfoAsync(string track, string artist, CancellationToken ct);
+    }
+}
